7e945b12135d9d19a2aa6d7688abdaf282b32f70
Avinaba <d97.avinaba@gmail.com>
2024-01-03 10:33:03 +0530

    Updated solution: Number of laser beams in a bank
    Signed-off-by: Avinaba <d97.avinaba@gmail.com>

--- arrays/NumberOfLaserBeams.java
+++ arrays/NumberOfLaserBeams.java
@@ -1,9 +1,10 @@
-// https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
+// https://leetcode.com/problems/number-of-laser-beams-in-a-bank
 class Solution {
     public int numberOfBeams(String[] bank) {
         int lastCount = 0, sum = 0;
         for (String s: bank) {
-            int ones = s.replace("0", "").length();
+            int ones = 0;
+            for(char c: s.toCharArray()) ones += c - '0';
             if (ones != 0) {
                 if (lastCount != 0) {
                     sum += lastCount * ones;
