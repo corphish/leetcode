b15d9381d5b1578dcb897f5bb1f05761d2368841
Avinaba Dalal <d97.avinaba@gmail.com>
2024-02-09 12:36:56 +0530

    solution: Decompress RLE List
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ arrays/DecompressRunLengthEncodedList.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/decompress-run-length-encoded-list/
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i += 2) {
+            for (int j = 0; j < nums[i]; j++) {
+                list.add(nums[i + 1]);
+            }
+        }
+
+        return list.stream().mapToInt(x -> x).toArray();
+    }
+}
