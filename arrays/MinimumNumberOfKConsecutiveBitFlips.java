d447129f41c5e3de68a51d247c074ca9257e25e4
Avinaba Dalal <d97.avinaba@gmail.com>
2024-07-03 10:37:54 +0530

    solution: Minimum number of K consecutive bit flips
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ arrays/MinimumNumberOfKConsecutiveBitFlips.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        int count = 0, soFar = 0;
+        int[] flips = new int[nums.length];
+        boolean[] wasFlipped = new boolean[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= nums.length - k) {
+                if (i < k) {
+                    if (nums[i] == soFar % 2) {
+                        soFar += 1;
+                        count += 1;
+                        wasFlipped[i] = true;
+                    }
+
+                    flips[i] = soFar;
+                } else {
+                    if (wasFlipped[i - k]) {
+                        soFar -= 1;
+                    }
+                    if (nums[i] == soFar % 2) {
+                        soFar += 1;
+                        count += 1;
+                        wasFlipped[i] = true;
+                    }
+                    flips[i] = soFar;
+                }
+            } else {
+                if (i >= k) {
+                    if (wasFlipped[i - k]) {
+                        soFar -= 1;
+                    }
+                    
+                    flips[i] = soFar;
+                } else {
+                   flips[i] = soFar;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == flips[i] % 2) {
+                return -1;
+            }
+        }
+
+        return count;
+    }
+}
