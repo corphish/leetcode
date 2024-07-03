b5ac4dc688db71c6696bc04a5b26a3138d443af6
Avinaba Dalal <d97.avinaba@gmail.com>
2024-07-03 10:39:04 +0530

    solution: Three consecutive odds
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ arrays/ThreeConsecutiveOdds.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/three-consecutive-odds
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if (arr.length < 3) {
+            return false;
+        }
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i - 1] % 2 == 1 && arr[i] % 2 == 1 && arr[i + 1] % 2 == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
