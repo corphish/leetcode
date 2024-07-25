eea74abf54adfeff5bbb8c2d4d1155db68c78b62
Avinaba Dalal <d97.avinaba@gmail.com>
2024-07-25 15:32:30 +0530

    solution: Alternating colors2
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ arrays/AlternatingColors2.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/alternating-groups-ii/
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int l = 0, count = 0;
+        int[] circ = new int[colors.length + k - 1];
+
+        for (int i = 0; i < circ.length; i++) {
+            if (i >= colors.length) {
+                circ[i] = colors[i % colors.length];
+            } else {
+                circ[i] = colors[i];
+            }
+        }
+
+        for (int i = 1; i < circ.length; i++) {
+            if (circ[i] == circ[i - 1]) {
+                l = i;
+            }
+
+            if (i - l == k - 1) {
+                count += 1;
+                l += 1;
+            }
+        }
+
+        return count;
+    }
+}
